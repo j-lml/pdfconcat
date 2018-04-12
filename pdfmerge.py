19390bc8eff444bb614eb7de641d0ef549f5f9f2
jlml <jesus.lopez@aytoarga.es>
2018-04-12 15:08:29 +0200

    creado version que no funciona

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+import datetime, calendar
+import glob
+import os
+import sys
+import fnmatch
+import ntpath
+import urllib
+import time
+import logging
+
+from shutil import copyfile
+import shutil
+
 from argparse import ArgumentParser
 from glob import glob
 from pyPdf import PdfFileReader, PdfFileWriter
+
+from subprocess import call
+import subprocess
 
 def merge(path, blank_filename, output_filename):
     blank = PdfFileReader(file(blank_filename, "rb"))
@@ -25,6 +42,106 @@ def merge(path, blank_filename, output_filename):
     output.write(output_stream)
     output_stream.close()
 
+def pdf_num_pages(pathfile):
+    document = PdfFileReader(open(pathfile, 'rb'))
+    return document.getNumPages()
+
+def pdf_is_odd(pathfile):
+    #es impar? si impar => poner pagina en blanco
+    flag=False
+    if ( pdf_num_pages(pathfile) % 2 == 1 ):
+        flag=True
+    return flag
+
+
+#devuelve array de nombres de ficheros (incluido el PATH) de forma recursiva
+#used: para buscar ficheros que ya existen
+def get_files(dirname, ext):
+    matches = []
+    for root, dirnames, filenames in os.walk(dirname):
+        for filename in fnmatch.filter(filenames, ext):
+            matches.append(os.path.join(root, filename))
+    return matches
+
+#devuelve la hoja (nombre del fichero) de cierto PATH
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+PATH_BASE='./pdf/'
+OUTPUT='output.pdf'
+
+def load_files(pathpdf):
+    #obtiene todos los ficheros de cierto tipo de forma recursiva
+    files= get_files(pathpdf, '*.pdf')
+    files= [path_leaf(path) for path in files]
+
+    return sorted(files)
+
+def exec_command(command):
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    process.wait();
+
+
+def merge_files(files):
+    for f in files:
+        filepath=PATH_BASE + f
+
+
+        #si no existe => crear el output con el fichero que corresponda
+        if not os.path.isfile(OUTPUT):
+            print('creando PDF: ' + f)
+            copyfile(filepath, OUTPUT)
+            continue
+
+        #comprobar num de paginas
+        fileout= OUTPUT
+        num_pages=pdf_num_pages(fileout)
+        add_pages=pdf_is_odd(fileout)
+
+        #si es impar => concatener con pagina
+        if add_pages == True:
+            #pdftk.exe output.pdf blank.pdf cat output output2.pdf
+            #pr=call(["pdftk.exe", OUTPUT, "blank.pdf", "cat", "output" , 'TMP_'+OUTPUT])
+
+            command="pdftk.exe " + OUTPUT +" blank.pdf cat output TMP_" + OUTPUT
+            exec_command(command)
+
+            # os.system("pdftk.exe " + OUTPUT +" blank.pdf cat output TMP_" + OUTPUT)
+            # os.close( 'TMP_'+OUTPUT )
+            # os.close( OUTPUT )
+            #os.rename('TMP_'+OUTPUT, OUTPUT)
+
+            shutil.copy("TMP_" + OUTPUT, OUTPUT)
+            os.remove("TMP_" + OUTPUT)
+
+            print('+new blank page')
+
+        #en cualquier caso => concatenar con el fichero
+        #pr=call(["pdftk.exe", OUTPUT, fileout, "cat", "output" , 'TMP_'+OUTPUT])
+        #pr.wait()
+        # os.system("pdftk.exe " + OUTPUT +" "+fileout+ " cat output TMP_" + OUTPUT)
+        # os.close( 'TMP_'+OUTPUT )
+        # os.close( OUTPUT )
+
+        command="pdftk.exe " + OUTPUT +" "+ fileout + " cat output TMP_" + OUTPUT
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        process.wait();
+
+        #os.rename('TMP_'+OUTPUT, OUTPUT)
+        # command="rename TMP_" + OUTPUT + " " + OUTPUT
+        # process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        # process.wait();
+
+        shutil.copy("TMP_" + OUTPUT, OUTPUT)
+        os.remove("TMP_" + OUTPUT)
+
+        print('+'+f)
+
+    print('end.')
+
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
 
@@ -37,4 +154,7 @@ if __name__ == "__main__":
                       help="path of source PDF files")
 
     args = parser.parse_args()
-    merge(args.path, args.blank_filename, args.output_filename)
+    #merge(args.path, args.blank_filename, args.output_filename)
+    files=load_files(PATH_BASE)
+    print( repr(files) )
+    merge_files(files)

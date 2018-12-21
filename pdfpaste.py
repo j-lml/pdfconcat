7461d44ac89d0bca928284c764dca730d7cd516d
jlml <jesus.lopez@aytoarga.es>
2018-12-21 14:28:14 +0100

    v1.0 genera estadisticas y corta pdf de 2016

--- pdfpaste.py
+++ pdfpaste.py
@@ -52,26 +52,6 @@ from msvcrt import kbhit
 #docker run -p 9998:9998 -it lexpredict/tika-server
 #
 
-def merge(path, blank_filename, output_filename):
-    blank = PdfFileReader(file(blank_filename, "rb"))
-    output = PdfFileWriter()
-
-    for pdffile in glob('*.pdf'):
-        if pdffile == output_filename:
-            continue
-        print("Parse '%s'" % pdffile)
-        document = PdfFileReader(open(pdffile, 'rb'))
-        for i in range(document.getNumPages()):
-            output.addPage(document.getPage(i))
-
-        if document.getNumPages() % 2 == 1:
-            output.addPage(blank.getPage(0))
-            print("Add blank page to '%s' (had %i pages)" % (pdffile, document.getNumPages()))
-    print("Start writing '%s'" % output_filename)
-    output_stream = file(output_filename, "wb")
-    output.write(output_stream)
-    output_stream.close()
-
 def pdf_num_pages(pathfile):
     document = PdfFileReader(open(pathfile, 'rb'))
     return document.getNumPages()
@@ -99,10 +79,6 @@ def path_leaf(path):
     return tail or ntpath.basename(head)
 
 
-OUTPUT_PATH='./cut/'
-OUTPUT='output.pdf'
-MAX_PAGES=100
-
 def load_files(pathpdf,type='*.pdf'):
     #obtiene todos los ficheros de cierto tipo de forma recursiva
     files= get_files(pathpdf,type )
@@ -110,47 +86,14 @@ def load_files(pathpdf,type='*.pdf'):
 
     return sorted(files)
 
+
 def exec_command(command):
-    process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE)
-    process.wait();
-    #os.system("start /wait cmd /c " +  command)
+    #ejecuta el comando SOLO si comando force -f
+    if FORCE==True:
+        process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE)
+        process.wait();
+        #os.system("start /wait cmd /c " +  command)
 
-cont=0
-PRE='TMP_'
-dest='TMP_0'
-
-def merge_file(first, second):
-    global cont
-    global dest
-
-    cont=cont+1
-    dest=PRE+str(cont)
-    command="pdftk.exe " + first + " " + second + " cat output " + dest
-    exec_command(command)
-    return dest
-
-
-#ref: https://stackoverflow.com/questions/6598937/set-output-location-for-pdftk-sample-pdf-burst
-#ref: pdftk burst https://www.pdflabs.com/docs/pdftk-man-page/#dest-op-burst
-def split_file(filename):
-    command="pdftk.exe " + filename + " burst output " + OUTPUT_PATH + "%04d.pdf"
-    exec_command(command)
-    return dest
-
-procfiles=0
-def tika_file(filename, inputpath="./cut/", outputpath='.'):
-    global procfiles
-
-    #tika-python -o . --server 10.10.10.1 --port 9998 parse text fichero.pdf
-    command="tika-python -o " + outputpath + " --server 10.60.6.165 --port 9998 parse text " + inputpath+filename
-
-    #si no existe O se fuerza =>
-    if os.path.isfile(outputpath+filename+"_meta.json")==False or FORCE==True:
-        procfiles=procfiles+1
-        exec_command(command)
-        print(command)
-
-    return dest
 
 def delete_files():
     files= get_files('.', 'TMP_*')
@@ -166,7 +109,13 @@ def get_bool(s):
         flag=True
     return flag
 
-items=[]
+#####################################################
+# PROGRAMA PRINCIPAL
+#####################################################
+
+
+#usado por proc_files()
+#sirve para cargar el fichero indicado en filename y realizar un primer analisis (si > 80 => true pe)
 def analize_file(filename, inputpath='./'):
     result=""
     with open(inputpath+filename, 'r') as handle:
@@ -189,13 +138,14 @@ def analize_file(filename, inputpath='./'):
     #print(j)
     return item
 
+#procedimiento ppal:
+#a partir de un array de ficheros => los analiza => genera bloques
 procfiles=0
-
-def proc_files(files):
+def proc_files(files, lastres, limit=0 ):
     global procfiles
     procfiles = 0
 
-    last=-1
+    last=lastres
 
     items=[]        #lista cont todos los ficheros
     blocklist=[]    #lista de bloques
@@ -203,38 +153,67 @@ def proc_files(files):
     blockerr=[]
 
     block=[]
-    expected=2016000001
+    first_expected=2016000001
+    expected=first_expected
+
+    limite=0
+
+    files=sorted(files)
+
+    firstgroup=False
+
+    store=[]
     for f in files:
         procfiles=procfiles+1
         result = analize_file(f,INPUT)
-        items.append(result) #almacena TODOS los ficheros procesados
-        #print(repr(result))
 
-        candidate=False
-        if result['first'] == True and result['resolution'] > last:
-            newgroup=True
+
+        items.append(result) #almacena TODOS los ficheros procesados
+        
+
+        #1. si empieza nuevo bloque => prepara bloque anterior
+        if result['first'] == True and result['resolution'] > last  and result['resolution'] >= expected:
+            firstgroup=True
             store=block
             block=[]
 
+        #1.1 saltar todos los que ya estan procesados en pdf ./result/
+        #si no empieza => continuar (solo comienza si result > last siendo last el ultimo pdf  )
+        if firstgroup==False:
+            continue
+
+        #2. almacena el nuevo bloque
         block.append(result)
 
+        #3. guarda el bloque anterior
         if len(store) >0:
             #almacenar el bloque antiguo y comenzar nuevo
             blocklist.append(store)
             new=store[0]['resolution']
             #si no es el esperado => anterior es error
-            if new != expected:
+            if new != expected and len(blockfine)>=1:
                 blockerr.append( blockfine[-1] )
             #si es >= a esperado => ok
             if new >= expected:
                 blockfine.append(store)
 
+            #salir si diferencia es muy grande!
+            if new > (expected+100) and expected != first_expected:
+                print("ERROR: diferencia muy grande en file " + store[0]['fjson'] + " " + str(expected) + " : " + str(new))
+                break
 
             store=[]
             expected=new+1
 
             #si es primero comieza bloque
             last=result['resolution']
+
+
+        #4. para poner limite (en pruebas). normalmente 10
+        limite=limite+1
+        if limite > limit and limit > 0:
+            break
+
 
 
 
@@ -251,24 +230,105 @@ def proc_files(files):
     return items,blocklist,blockfine,blockerr
 
 
+#a partir de un bloque de ficheros lo transforma en un pdf en outputpath
+#los bloques de fichero se obtienen en proc_files()
+def paste_pdf(block, outputpath='./result/', inputpath='./pdf/'):
+    cad=""
+
+    dest=outputpath + str(block[0]['resolution']) + ".pdf"
+    for item in block:
+        cad = cad + " " + inputpath + item['fpdf']
+    cad=cad.strip()
+    command="pdftk.exe " + cad + " " + " cat output " + dest
+
+    #con param -g se muestran los comandos a generar
+    #NECESITA param -f para que se ejecute el comando!
+    print(command)
+    exec_command(command)
+
+#muestra estadisticas de los bloques
+contfiles=0
+def statistics(items,blocks,bfine,berr):
+    print("statistics:")
+    cf=[x for x in items if x['first']==True]
+    cl=[x for x in items if x['last']==True]
+    cr=[x for x in items if x['resolution']!=None]
+    crl=[x['resolution'] for x in items if x['resolution']!=None]
+    crf=[x for x in items if x['resolution']!=None and x['first']==False]
+
+    #en el caso ideal => total first == total resol
+    print("total first: "  + str(len(cf)))
+    print("total last: "  + str(len(cl)))
+    print("total resol: "  + str(len(cr)))
+    print("total resol NO repes: "  + str(len(crl)))
+    print("total resol NO first: "  + str(len(crf)))
+
+    #ficheros en carpeta de entrada
+    print("ficheros json: "  + str(contfiles))
+    #ficheros procesados
+    print("ficheros procesados: "  + str(procfiles))
+
+    #sobre resultados:
+    temp=bfine[0]
+    #print(repr(temp))
+    min=bfine[0][0]['resolution']
+    max=bfine[-1][0]['resolution']
+
+    print("min detected: " + str(min))
+    print("max detected: " + str(max))
+    exp=max - min + 1
+    print("expected: " + str(exp))
+    det=len(bfine)
+    print("detected: " + str(det))
+    err=len(berr)
+    print("errors: " + str(err))
+    pdet= (float(det) * 100 ) / float(exp)
+    print("% detected: " + str(pdet))
+    perr= (float(err) * 100 ) / float(exp)
+    print("% errores: " + str(perr))
+
+
 
 
 if __name__ == "__main__":
+    #ej: python pdfpaste.py -i .\json2\  -o .\result\ -g
+    #
+    #dirs por defecto:
+    # -i ./json/ => los extractos con la metainformacion
+    # -o ./result/ => donde se almacena resultando
+    #    ./result_err/ => mismo que -o con _err (donde se almacenan erroneos)
+    # -p ./pdf/ => donde se encuentran los pdf a concatenar
+    # -g -> crea comandos de pegado
+    # -f => realiza la creacion de documentos!
+    # -s => muestra estadisticas
+    #con param -g se muestran los comandos a generar
+    #nota: NECESITA param -f para que se ejecute el comando!
+
+
     parser = ArgumentParser()
 
     # Add more options if you like
     parser.add_argument("-i", "--input", dest="input_path", default="./json2/",
-                      help="path de tiff o pdf", metavar="PATH")
+                      help="path con json", metavar="PATH")
     parser.add_argument("-o", "--output", dest="output_path", default="./result/",
                       help="path de salida", metavar="PATH")
+    parser.add_argument("-p", "--pdf", dest="pdf_path", default="./pdf/",
+                    help="path de de los pdf", metavar="PATH")
     parser.add_argument("-f", "--force", action='store_true',
                       help="force overwrite")
+    parser.add_argument("-g", "--generate", action='store_true',
+                      help="force overwrite")
+    parser.add_argument("-s", "--statistics", action='store_true',
+                    help="muestra estadisticas")
     args = parser.parse_args()
 
 
     INPUT=args.input_path
     OUTPUT_PATH=args.output_path
+    PDF_PATH=args.pdf_path
     FORCE=get_bool(args.force)
+    GENERATE=get_bool(args.generate)
+    STATS=get_bool(args.statistics)
 
     #result=load_files(INPUT,'*.tiff')
     print("loading files...")
@@ -276,28 +336,44 @@ if __name__ == "__main__":
     contfiles=len(result)
     print("loaded: " + str( contfiles) )
 
-    print("start")
-    items,blocks,bfine,berr=proc_files(result)
+    #result=load_files(INPUT,'*.tiff')
+    print("loading pdf...")
+    pdfs=load_files(OUTPUT_PATH,'*.pdf')
+    pdfs=sorted(pdfs)
+    contpdf=len(pdfs)
+    print("loaded: " + str( contpdf) )
+    lastpdf=pdfs[-1] if len(pdfs)>=1 else ""
+    print("last pdf: " + lastpdf)
+
+    lastres=-1
+    try:
+        lastres=int(lastpdf.split('.')[0])
+    except:
+        print('no encontrado resolucion pdf '  + OUTPUT_PATH)
+
+    print("last pdf: " + lastpdf)
+    print("last res: " + str(lastres))
+
+
+
+    print("processing files")
+    items,blocks,bfine,berr=proc_files(result,lastres)
     print("end")
 
-    cf=[x for x in items if x['first']==True]
-    cl=[x for x in items if x['last']==True]
-    cr=[x for x in items if x['resolution']!=None]
-    crl=[x['resolution'] for x in items if x['resolution']!=None]
-    crf=[x for x in items if x['resolution']!=None and x['first']==False]
-    print("total first: "  + str(len(cf)))
-    print("total last: "  + str(len(cl)))
-    print("total resol: "  + str(len(cr)))
-    print("total resol NO repes: "  + str(len(crl)))
-    #print(repr(crl))
-    print("total resol NO first: "  + str(len(crf)))
-    #print(repr(crf))
+    if GENERATE == True:
+        print("pdf:")
+        for f in bfine:
+            paste_pdf(f,OUTPUT_PATH, PDF_PATH)
 
-    #result=analize_file( '00011.json', INPUT)
-    #print(repr(result))
-    print("total ficheros: "  + str(contfiles))
-    print("ficheros procesados: "  + str(procfiles))
+        print("errors:")
+        for f in berr:
+            paste_pdf(f,OUTPUT_PATH[:-1]+'_err/', PDF_PATH)
 
+
+    if STATS == True:
+        statistics(items,blocks,bfine,berr)
+
+    '''
     print("--------------")
     for f in blocks:
         print("res: " + str(f[0]['resolution']) )
@@ -315,3 +391,4 @@ if __name__ == "__main__":
         print("err: " + str(f[0]['resolution']) )
         fine=[x['fpdf'] for x in f]
         print(repr(fine))
+    '''
